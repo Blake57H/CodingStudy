35cf42cd334683ba59a45d604c9d51a592fc24f3
Blake57H <43494209+Blake57H@users.noreply.github.com>
2024-09-08 22:42:43 +0800

    Try adding image to 30's readme

--- Blog/Daily Coding Problem/30/main.cpp
+++ Blog/Daily Coding Problem/30/main.cpp
@@ -1,11 +1,62 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <format>
+#include <sstream>
 
-int solve(){
-    return 0;
+int solve(const std::vector<size_t> &map)
+{
+    if (map.size() == 0)
+        return 0;
+
+    // size of map is at least 1,
+    size_t volume = 0;
+    std::vector<size_t>::const_iterator start_pos = map.begin();
+    for (auto pos = map.begin() + 1; pos != map.end(); pos++)
+    {
+        // assume code won't reach here wen pos==map.begin()
+        if (pos + 1 == map.end() || (*pos > *(pos - 1) && *(pos + 1) < *pos))
+        {
+            // marking ending height (between starting pos and ending pos, a valley will be formed)
+            // start calculating how much water can be trapped in this valley
+            auto shorter_height = std::min(*start_pos, *pos);
+            for (auto i = start_pos + 1; i < pos; i++)
+            {
+                if (*i < shorter_height)
+                    volume += shorter_height - *i;
+            }
+            start_pos = pos;
+        }
+    }
+
+    return volume;
 }
 
-int main(int argc, char* argv[]){
-    auto result = solve();
-    std::cout << "Result: " << result << "\n";
+std::string print_map(const std::vector<size_t> &map)
+{
+    std::ostringstream oss;
+    oss << '[';
+    for (auto &i : map)
+    {
+        oss << i;
+        if (&i != &map.back())
+            oss << ',' << ' ';
+    }
+    oss << ']';
+    return oss.str();
+}
+
+int main(int argc, char *argv[])
+{
+    auto maps = std::vector<std::vector<size_t>>{
+        {2, 1, 2},
+        {3, 0, 1, 3, 0, 5},
+        {3, 0, 1, 4, 4, 4},
+        {3, 0, 1, 4, 3, 4},
+        {3, 0, 1, 4, 3, 2},
+        {0, 0, 0, 0, 0, 0, 0},
+        {3, 0, 1, 4, 5, 4}};
+    for (auto map : maps)
+        std::cout << std::format("Result of {}: {}\n", print_map(map), solve(map));
     return 0;
 }
--- Blog/Daily Coding Problem/30/readme.md
+++ Blog/Daily Coding Problem/30/readme.md
@@ -20,18 +20,22 @@ I spent quite some time to understand what the problem is. I couldn't understand
 
 Imagine a map, where there's 3 trunks of floor. The floor's height is 2 units on the left, 1 unit on the middle, and 2 on the right. Therefore 1 unit of water will be trapped in this map.
 
+![Image of map (2,1,2)](30-1.png)
+
 >> Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in the second, and 3 in the fourth index (we cannot hold 5 since it would run off to the left), so we can trap 8 units of water.
 
 I drew a map of this example.
 
-1. 
-2. 
-3. 
-4. 
-5. 
+![Image of map (3, 0, 1, 3, 0, 5)](30-2.png)
+
+And to solve this problem I imagine I need the program to know where the curve is going. Begin from the first floor, is the floor height going up or going down?
+
+Update 1: I think I only need the position of the top of the hills (the place where floors on its left and right are lower).
 
 ----
 
-O(?) time complexity and O(?) space complexity.
+Array is iterated (at most) 2 times.
+
+O(n) time complexity and O(1) space complexity.
 
 ## Googled solution (if I actually googled it)
